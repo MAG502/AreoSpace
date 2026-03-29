28b98e15eb76703e47c54a8a93803d0ac30d23a4
MAG502 <mohammeddos@icloud.com>
2026-03-29 14:51:01 +0300

    Add files via upload
    printing autopilot name

---
+++ README.md
@@ -0,0 +1,3 @@
+This project so far tells us the name of the drone conncted via simulator.
+
+once the sumulator is running run the file and it will print the name of the autopilot
---
+++ print_Autopilot_name.py
@@ -0,0 +1,34 @@
+from pymavlink import mavutil
+
+
+master = mavutil.mavlink_connection('tcp:127.0.0.1:5763')
+
+print(master.wait_heartbeat())
+
+autopilot_value = master.messages['HEARTBEAT'].autopilot
+
+autopilots = {
+    0: "GENERIC",
+    1: "RESERVED",
+    2: "SLUGS",
+    3: "ARDUPILOTMEGA",
+    4: "OPENPILOT",
+    5: "GENERIC_WAYPOINTS_ONLY",
+    6: "GENERIC_WAYPOINTS_AND_SIMPLE_NAVIGATION_ONLY",
+    7: "GENERIC_MISSION_FULL",
+    8: "INVALID",
+    9: "PPZ",
+    10: "UDB",
+    11: "FP",
+    12: "PX4",
+    13: "SMACCMPILOT",
+    14: "AUTOQUAD",
+    15: "ARMAZILA",
+    16: "AEROB",
+    17: "ASLUAV",
+    18: "SMARTAP",
+    19: "AIRRAILS",
+    20: "REFLEX"
+}
+
+print("Autopilot Name:", autopilots[autopilot_value])
