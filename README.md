92c3d0b4b05f66dceb34f959ea4ac9671e524eae
Mohammed <mohammeddos@icloud.com>
2026-03-31 11:28:25 +0300

    done with task 3

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-File : print_Autopilot_name.py
+File: print_Autopilot_name.py
 This file tells us the name of the drone conncted via simulator.
 
 once the sumulator is running run the file and it will print the name of the autopilot.
 
-File : print_Autopilot_pressure.py
+File: print_Autopilot_pressure.py
 this file tells us how many pressure sensors are Dedicated.
+
+File: print_num_of_imu.py
+this file tells us how many imu sensors are dedicted.
---
+++ print_num_of_imu.py
@@ -0,0 +1,38 @@
+from pymavlink import mavutil
+
+master = mavutil.mavlink_connection('udp:127.0.0.1:14550')
+
+master.wait_heartbeat()
+print('Got Heartbeat\n')
+
+master.mav.command_long_send(
+    master.target_system,
+    master.target_component,
+    mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,
+    0,
+    mavutil.mavlink.MAVLINK_MSG_ID_SCALED_IMU,
+    1000000,
+    0, 0, 0, 0, 0
+)
+
+imu_senesor_values = master.recv_match(type='SCALED_IMU', blocking=True, timeout=1)
+imu_senesor_values2 = master.recv_match(type='SCALED_IMU2', blocking=True, timeout=1)
+imu_senesor_values3 = master.recv_match(type='SCALED_IMU3', blocking=True, timeout=1)
+
+number_of_senesors = 0
+
+pr1, pr2, pr3 = "", "", ""
+if imu_senesor_values:
+    pr1 = f'Sensor {imu_senesor_values.msgname.replace("SCALED_", "")} dedicated'
+    number_of_senesors += 1
+
+if imu_senesor_values2:
+    pr2 = f'\nSensor {imu_senesor_values2.msgname.replace("SCALED_", "")} dedicated'
+    number_of_senesors += 1
+    
+if imu_senesor_values3:
+    pr3 = f'\nSensor {imu_senesor_values3.msgname.replace("SCALED_", "")} dedicated'
+    number_of_senesors += 1
+
+print(pr1 + pr2 + pr3)
+print(f'The Number Of Senesors Dedicated: {number_of_senesors}')
