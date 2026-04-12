1d2b80d2e7a77f8cee650f6107886d981ee9db40
Mohammed <mohammeddos@icloud.com>
2026-04-12 09:33:48 +0300

    sensors_parameters update

--- sensors_parameters.py
+++ sensors_parameters.py
@@ -3,91 +3,17 @@
 from pymavlink import mavutil
 from static import baro_types, bustypes, compass_types, imu_types, airspeed_types
 
-# master = mavutil.mavlink_connection('tcp:127.0.0.1:5763')
-master = mavutil.mavlink_connection('/dev/tty.usbmodem101')
+master = mavutil.mavlink_connection('tcp:127.0.0.1:5763')
+# master = mavutil.mavlink_connection('/dev/tty.usbmodem101')
 
 master.wait_heartbeat()
 print('Got Heartbeat.')
 
-
-
-def baro_param(master):
-    master.param_fetch_one('BARO1_DEVID')
-    master.param_fetch_one('BARO2_DEVID')
-    master.param_fetch_one('BARO3_DEVID')
-
-    param1 = master.recv_match(type='PARAM_VALUE', blocking=True, timeout=10)
-    param2 = master.recv_match(type='PARAM_VALUE', blocking=True, timeout=10)
-    param3 = master.recv_match(type='PARAM_VALUE', blocking=True, timeout=10)
-    
-    param_name1, param_value1 = param1.param_id, param1.param_value
-    param_name2, param_value2 = param2.param_id, param2.param_value
-    param_name3, param_value3 = param3.param_id, param3.param_value
-
-    prints(param_name1, param_value1)
-    prints(param_name2, param_value2)
-    prints(param_name3, param_value3)
-
-
-def compass_param(master):
-    master.param_fetch_one('COMPASS_DEV_ID')
-    master.param_fetch_one('COMPASS_DEV_ID2')
-    master.param_fetch_one('COMPASS_DEV_ID3')
-
-    param1 = master.recv_match(type='PARAM_VALUE', blocking=True, timeout=1)
-    param2 = master.recv_match(type='PARAM_VALUE', blocking=True, timeout=1)
-    param3 = master.recv_match(type='PARAM_VALUE', blocking=True, timeout=1)
-    
-    param_name1, param_value1 = param1.param_id, param1.param_value
-    param_name2, param_value2 = param2.param_id, param2.param_value
-    param_name3, param_value3 = param3.param_id, param3.param_value
-    
-    prints(param_name1, param_value1)
-    prints(param_name2, param_value2)
-    prints(param_name3, param_value3)
-
-def ins_acc_param(master):
-    master.param_fetch_one('INS_ACC_ID')
-    master.param_fetch_one('INS_ACC2_ID')
-    master.param_fetch_one('INS_ACC3_ID')
-
-    param1 = master.recv_match(type='PARAM_VALUE', blocking=True, timeout=1)
-    param2 = master.recv_match(type='PARAM_VALUE', blocking=True, timeout=1)
-    param3 = master.recv_match(type='PARAM_VALUE', blocking=True, timeout=1)
-
-    param_name1, param_value1 = param1.param_id, param1.param_value
-    param_name2, param_value2 = param2.param_id, param2.param_value
-    param_name3, param_value3 = param3.param_id, param3.param_value
-
-    prints(param_name1, param_value1)
-    prints(param_name2, param_value2)
-    prints(param_name3, param_value3)
-
-def ins_gyr_param(master):
-    master.param_fetch_one('INS_GYR_ID')
-    master.param_fetch_one('INS_GYR2_ID')
-    master.param_fetch_one('INS_GYR3_ID')
-
-    param1 = master.recv_match(type='PARAM_VALUE', blocking=True, timeout=1)
-    param2 = master.recv_match(type='PARAM_VALUE', blocking=True, timeout=1)
-    param3 = master.recv_match(type='PARAM_VALUE', blocking=True, timeout=1)
-
-    param_name1, param_value1 = param1.param_id, param1.param_value
-    param_name2, param_value2 = param2.param_id, param2.param_value
-    param_name3, param_value3 = param3.param_id, param3.param_value
-
-    prints(param_name1, param_value1)
-    prints(param_name2, param_value2)
-    prints(param_name3, param_value3)
-
-def arspd_param(master):
-    master.param_fetch_one('ARSPD_DEVID')
-
-    param = master.recv_match(type='PARAM_VALUE', blocking=True, timeout=1)
-
-    param_name, param_value = param.param_id, param.param_value
-
-    prints(param_name, param_value)
+def fetch(master, param_list):
+    for baro in param_list:
+        master.param_fetch_one(baro)
+        param = master.recv_match(type='PARAM_VALUE', blocking=True, timeout=1)
+        prints(param.param_id, param.param_value)
 
 def prints(param_id, param_value):
     
@@ -97,6 +23,7 @@ def prints(param_id, param_value):
     address = (param_value >> 8) & 0b11111111 #0xFF
     sensor_model = (param_value >> 16)
     types = {}
+
     if param_id.startswith('BARO'):
         types = baro_types
     
@@ -122,28 +49,33 @@ while True:
     name = input('Pleas Enter Sensor Name: ')
     
     if name.startswith(('BARO', 'baro')):
-        baro_param(master)
+        baro_param = ['BARO1_DEVID', 'BARO2_DEVID', 'BARO3_DEVID']
+        fetch(master, baro_param)
         number_of_try = 0
     
     elif name.startswith(('COMPASS', 'compass')):
-        compass_param(master)
+        compass_param = ['COMPASS_DEV_ID', 'COMPASS_DEV_ID2', 'COMPASS_DEV_ID3']
+        fetch(master, compass_param)
         number_of_try = 0
     
     elif name.startswith(('INS_ACC', 'ins_acc')):
-        ins_acc_param(master)
+        ins_acc_param = ['INS_ACC_ID', 'INS_ACC2_ID', 'INS_ACC3_ID']
+        fetch(master, ins_acc_param)
         number_of_try = 0
 
     elif name.startswith(('INS_GYR', 'ins_gyr')):
-        ins_gyr_param(master)
+        ins_gyr_param = ['INS_GYR_ID', 'INS_GYR2_ID', 'INS_GYR3_ID']
+        fetch(master, ins_gyr_param)
         number_of_try = 0
         
     elif name.startswith(('ARSPD', 'arspd')):
-        arspd_param(master)
+        arspd_param = ['ARSPD_DEVID']
+        fetch(master, arspd_param)
         number_of_try = 0
     
     else:
         print('\nPleas Enter Valid Sensor Name.\nNames: BARO, COMPASS, INS_ACC, INS_GYR, ARSPD\n')
         number_of_try += 1
-        if number_of_try > 3:
-            print('Excited Max Number Of Tries.')
+        if number_of_try > 2:
+            print('Exciting Max Number Of Tries.')
             break
